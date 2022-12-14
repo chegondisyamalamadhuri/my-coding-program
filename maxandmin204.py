69717405d1d0838ce91ca51573755437db007e02
chegondisyamalamadhuri <111191806+chegondisyamalamadhuri@users.noreply.github.com>
2022-12-14 20:36:45 +0530

    Update maxandmin204.py

--- maxandmin204.py
+++ maxandmin204.py
@@ -1,12 +1,12 @@
 l=[]
 n=int(input("enter n value"))
 l=list(map(int,input().split()))
-max=l[0]
-min=l[0]
+maxi=l[0]
+mini=l[0]
 for i in range(1,n):
-    if l[i]>max:
-        max=l[i]
+    if l[i]>maxi:
+        maxi=l[i]
     else:
-        min=l[i]
-print(max)
-print(min)
+        mini=l[i]
+print(maxi)
+print(mini)
