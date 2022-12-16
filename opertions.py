f50d03264790c263e62d2561d2c9f61310ae6009
chegondisyamalamadhuri <111191806+chegondisyamalamadhuri@users.noreply.github.com>
2022-12-16 20:18:44 +0530

    Create opertions.py

---
+++ opertions.py
@@ -0,0 +1,16 @@
+str1=input()
+length=len(str1)
+print("no.of character of string=",length)
+
+
+str2=input()
+print(str2.upper())
+
+
+str3=input()
+print(str3.lower())
+
+
+
+str4=input()
+print(str4.swapcase())
