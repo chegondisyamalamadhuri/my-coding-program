ffbe53551a128a55ac6a4be604852bcc5d06708f
chegondisyamalamadhuri <111191806+chegondisyamalamadhuri@users.noreply.github.com>
2022-12-25 16:04:16 +0530

    Create assignment5.py

---
+++ assignment5.py
@@ -0,0 +1,14 @@
+n = int(input())
+flag = 0
+i = 2
+while i <= (n / 2):
+    if (n % i) == 0:
+        flag = 1
+        break
+    else:
+        i += 1
+
+if flag == 0:
+    print(n, ' is a prime number.')
+elif flag==1:
+    print(n, ' is not a prime number.')
