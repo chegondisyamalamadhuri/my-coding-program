27a9eb15cc9ac750315e90db4244b6184485f136
chegondisyamalamadhuri <111191806+chegondisyamalamadhuri@users.noreply.github.com>
2023-01-06 21:23:58 +0530

    Create emailcompany.py

---
+++ emailcompany.py
@@ -0,0 +1,5 @@
+email=input()
+if email.endswith("@gmail.com") or email.endswith("@yahoo.com") or email.endswith("@adc.aditya.ac.in"):
+    print("you are using google account")
+else:
+    print("Invalid Email id")
